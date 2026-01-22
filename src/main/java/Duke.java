bdd08821c066392a867ed90b98ad6e52720cdf4d
shrievarshanbaskar <shrievarshan@gmail.com>
2026-01-22 22:38:18 +0800

    Complete Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,85 +15,79 @@ public class Duke {
         while (true) {
             String input = sc.nextLine();
 
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.todotoprintln("------------------------------------------------");
-                break;
-            }
-
-            else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                Task task = list.get(index);
-                task.setStatus(true);
-
-                System.out.println("-------------------------------------------------");
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + task);
-                System.out.println("-------------------------------------------------");
-            }
-
-            else if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.substring(7)) - 1;
-                Task task = list.get(index);
-                task.setStatus(false);
-
-                System.out.println("-------------------------------------------------");
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + task);
-                System.out.println("-------------------------------------------------");
-            }
-
-            else if (input.equals("list")) {
-                System.out.println("------------------------------------------------");
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". " + list.get(i));
+            try {
+                if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("------------------------------------------------");
+                    break;
                 }
-                System.out.println("------------------------------------------------");
-            }
 
-            else if (input.startsWith("todo ")) {
-                String name = input.substring(5);
-                Task task = new Todo(name, false);
-                list.add(task);
+                else if (input.equals("todo") || input.length() <= 5 && input.startsWith("todo")) {
+                    throw new DukeException("The description of a todo cannot be empty.");
+                }
 
+                else if (input.startsWith("todo ")) {
+                    String name = input.substring(5);
+                    Task task = new Todo(name, false);
+                    list.add(task);
+
+                    System.out.println("-------------------------------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + task);
+                    System.out.println("Now you have " + list.size() + " tasks in the list.");
+                    System.out.println("-------------------------------------------------");
+                }
+
+                else if (input.startsWith("deadline ")) {
+                    if (!input.contains(" /by ")) {
+                        throw new DukeException("A deadline must have a /by.");
+                    }
+
+                    String[] parts = input.substring(9).split(" /by ");
+                    Task task = new Deadline(parts[0], false, parts[1]);
+                    list.add(task);
+
+                    System.out.println("-------------------------------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + task);
+                    System.out.println("Now you have " + list.size() + " tasks in the list.");
+                    System.out.println("-------------------------------------------------");
+                }
+
+                else if (input.startsWith("event ")) {
+                    if (!input.contains(" /from ") || !input.contains(" /to ")) {
+                        throw new DukeException("An event must have /from and /to.");
+                    }
+
+                    String[] first = input.substring(6).split(" /from ");
+                    String[] second = first[1].split(" /to ");
+
+                    Task task = new Event(first[0], false, second[0], second[1]);
+                    list.add(task);
+
+                    System.out.println("-------------------------------------------------");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + task);
+                    System.out.println("Now you have " + list.size() + " tasks in the list.");
+                    System.out.println("-------------------------------------------------");
+                }
+
+                else if (input.equals("list")) {
+                    System.out.println("------------------------------------------------");
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println((i + 1) + ". " + list.get(i));
+                    }
+                    System.out.println("------------------------------------------------");
+                }
+
+                else {
+                    throw new DukeException("I'm sorry, but I don't know what that means.");
+                }
+
+            } catch (DukeException e) {
                 System.out.println("-------------------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + task);
-                System.out.println("Now you have " + list.size() + " tasks in the list.");
-                System.out.println("-------------------------------------------------");
-            }
-
-            else if (input.startsWith("deadline ")) {
-                String[] parts = input.substring(9).split(" /by ");
-                String name = parts[0];
-                String by = parts[1];
-
-                Task task = new Deadline(name, false, by);
-                list.add(task);
-
-                System.out.println("-------------------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + task);
-                System.out.println("Now you have " + list.size() + " tasks in the list.");
-                System.out.println("-------------------------------------------------");
-            }
-
-            else if (input.startsWith("event ")) {
-                String[] firstSplit = input.substring(6).split(" /from ");
-                String name = firstSplit[0];
-
-                String[] secondSplit = firstSplit[1].split(" /to ");
-                String from = secondSplit[0];
-                String to = secondSplit[1];
-
-                Task task = new Event(name, false, from, to);
-                list.add(task);
-
-                System.out.println("-------------------------------------------------");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + task);
-                System.out.println("Now you have " + list.size() + " tasks in the list.");
+                System.out.println("OOPS!! " + e.getMessage());
                 System.out.println("-------------------------------------------------");
             }
         }
