0fbf7656775c170a392b27d1752835f7ec5663b6
shrievarshanbaskar <shrievarshan@gmail.com>
2026-01-22 18:08:42 +0800

    Complete Level-1

--- src/main/java/Fozza.java
+++ src/main/java/Fozza.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Fozza {
     public static void main(String[] args) {
 
@@ -5,7 +7,21 @@ public class Fozza {
         System.out.println("Hello! I'm Fozza");
         System.out.println("What can I do for you?\n");
         System.out.println("-------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("------------------------------------------------");
-    }
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("------------------------------------------------");
+                break;
+            } else {
+                System.out.println("-------------------------------------------------");
+                System.out.println("  " + input);
+                System.out.println("-------------------------------------------------");
+            }
+
+            }
+
+        }
 }
