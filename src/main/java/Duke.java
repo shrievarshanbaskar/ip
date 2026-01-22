72788b043e7f89aad9645487e5268c6976882b33
shrievarshanbaskar <shrievarshan@gmail.com>
2026-01-22 19:33:06 +0800

    Complete Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke{
         System.out.println("-------------------------------------------------");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
 
         while (true) {
@@ -20,16 +20,37 @@ public class Duke{
                 System.out.println("Bye. Hope to see you again soon!\n");
                 System.out.println("------------------------------------------------");
                 break;
-            } else if (input.equals("list")) {
+            }
+            else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                Task task = list.get(index);
+                task.setStatus(true);
+
+                System.out.println("-------------------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
+                System.out.println("-------------------------------------------------");
+            }
+            else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                Task task = list.get(index);
+                task.setStatus(false);
+
+                System.out.println("-------------------------------------------------");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task);
+                System.out.println("-------------------------------------------------");
+            }
+            else if (input.equals("list")) {
                 System.out.println("------------------------------------------------");
                 for(int i = 0; i<list.size(); i++) {
-                    System.out.println(i+1 +". " + list.get(i));
+                    System.out.println(i+1 +". " + list.get(i).toString());
                 }
                 System.out.println("------------------------------------------------");
 
             } else {
 
-                list.add(input);
+                list.add(new Task(input, false));
                 System.out.println("-------------------------------------------------");
                 System.out.println("  " + input);
                 System.out.println("-------------------------------------------------");
@@ -39,3 +60,4 @@ public class Duke{
 
         }
 }
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private String name;
+    private boolean status;
+
+    public Task(String name, boolean status) {
+        this.name = name;
+        this.status = status;
+    }
+    public boolean getStatus() {
+        return this.status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    @Override
+    public String toString() {
+        if(status) {
+            return "[X] " + this.name;
+        }
+        return "[ ] " + this.name;
+    }
+
+}
