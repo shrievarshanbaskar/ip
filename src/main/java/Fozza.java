4603eb804d010db5b91d3acc4448fa08921522f2
shrievarshanbaskar <shrievarshan@gmail.com>
2026-01-22 17:18:07 +0800

    Complete Level-0

---
+++ src/main/java/Fozza.java
@@ -0,0 +1,11 @@
+public class Fozza {
+    public static void main(String[] args) {
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("Hello! I'm Fozza");
+        System.out.println("What can I do for you?\n");
+        System.out.println("-------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("------------------------------------------------");
+    }
+}
