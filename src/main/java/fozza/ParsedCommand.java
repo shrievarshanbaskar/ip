472c62dec0d7283620eeb6611b3d749849899fba
shrievarshanbaskar <shrievarshan@gmail.com>
2026-01-29 01:45:37 +0800

    Fix ParsedCommand visibility and accessors

--- src/main/java/fozza/ParsedCommand.java
+++ src/main/java/fozza/ParsedCommand.java
@@ -1,15 +1,48 @@
 package fozza;
 
 public class ParsedCommand {
-    public final CommandType type;
-    public final String a;
-    public final String b;
-    public final String c;
+
+    public CommandType type;
+    public String a;
+    public String b;
+    public String c;
+
+    public ParsedCommand(CommandType type) {
+        this.type = type;
+    }
+
+    public ParsedCommand(CommandType type, String a) {
+        this.type = type;
+        this.a = a;
+    }
+
+    public ParsedCommand(CommandType type, String a, String b) {
+        this.type = type;
+        this.a = a;
+        this.b = b;
+    }
 
     public ParsedCommand(CommandType type, String a, String b, String c) {
         this.type = type;
         this.a = a;
         this.b = b;
         this.c = c;
+    }
+
+    // ===== getters for JUnit =====
+    public CommandType getCommandType() {
+        return type;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public String getC() {
+        return c;
     }
 }
